99607e471ff8a4730ccda575152772728785ba95
448 OG <superuser@448.africa>
2024-12-07 11:49:22 +0300

    Update template to use JS wallet adapter theme and dioxus 0.6

---
+++ dioxus/src/views/dashboard_disconnected.rs
@@ -0,0 +1,39 @@
+use dioxus::prelude::*;
+use dioxus_logger::tracing::Level;
+use wallet_adapter::WalletAdapter;
+
+use crate::{Footer, Route};
+
+pub fn WalletDashboard() -> Element {
+    rsx! {
+        div {class:"flex-grow mx-4 lg:mx-auto",
+            div{
+                div {class:"hero py-[64px]",
+                    div {class:"hero-content text-center",
+                        div {class:"max-w-2xl",
+                            h1 {class:"text-5xl font-bold", "gm"}
+                            p {class:"py-6", "Say hi to your new Solana dApp."}
+                        }
+                    }
+                }
+                div {class:"max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center",
+                    div {class:"space-y-2",
+                        p{"Here are some helpful links to get you started."}
+                        div{ a {href:"https://docs.solana.com/", class:"link", target:"_blank",
+                                rel:"noopener noreferrer", "Solana Docs"}}
+                        div{ a {href:"https://faucet.solana.com/", class:"link", target:"_blank",
+                                rel:"noopener noreferrer", "Solana Faucet" }}
+                        div{ a {href:"https://solanacookbook.com/", class:"link", target:"_blank",
+                                rel:"noopener noreferrer", "Solana Cookbook" }}
+                        div{ a {href:"https://solana.stackexchange.com/", class:"link", target:"_blank",
+                                rel:"noopener noreferrer", "Solana Stack Overflow" }}
+                        div{ a {href:"https://github.com/solana-developers/", class:"link", target:"_blank",
+                                rel:"noopener noreferrer", "Solana Developers GitHub" }}
+                    }
+                }
+            }
+            div {style:"position: fixed; z-index: 9999; inset: 16px; pointer-events: none;"}
+        }
+        Footer{}
+    }
+}
