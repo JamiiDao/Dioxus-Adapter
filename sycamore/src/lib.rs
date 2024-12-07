fe20b1f4513032e69247968599077e76225539fb
448 OG <superuser@448.africa>
2024-12-07 18:23:48 +0300

    Model the sycamore template after the new example

--- sycamore/src/lib.rs
+++ sycamore/src/lib.rs
@@ -1,121 +1,55 @@
 use sycamore::prelude::*;
-use wallet_adapter::{InitEvents, Wallet, WalletAccount, WalletStorage};
+use sycamore_router::{HistoryIntegration, Route, Router};
+use wallet_adapter::WalletAdapter;
 use wasm_bindgen::prelude::*;
-use web_sys::{HtmlElement, MouseEvent};
 
 mod header;
 use header::*;
+
+mod homepage;
+use homepage::*;
+
+mod footer;
+pub(crate) use footer::*;
 
 #[wasm_bindgen(start)]
 pub fn main() {
     sycamore::render(App);
 }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
-struct SycamoreWalletAdapter {
-    storage: Signal<WalletStorage>,
-    connected: Signal<Option<ConnectedArgs>>,
-}
-
-impl SycamoreWalletAdapter {
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
-pub struct ConnectedArgs {
-    pub(crate) connected_account: WalletAccount,
-    pub(crate) connected_wallet: Wallet,
-}
-
-#[derive(Debug, Props)]
-pub struct Controller<'a> {
-    pub(crate) connected_account: &'a WalletAccount,
-    pub(crate) connected_wallet: &'a Wallet,
-}
-
 #[component]
 fn App() -> View {
-    let adapter = SycamoreWalletAdapter::new();
+    let adapter = WalletAdapter::init().unwrap();
 
-    RootPage(adapter)
-}
-
-#[component]
-fn RootPage(adapter: SycamoreWalletAdapter) -> View {
-    let mut storage = WalletStorage::default();
-
-    InitEvents::new(&window()).init(&mut storage).unwrap();
-    adapter.storage.set(storage);
-
-    let show_modal = create_signal(false);
+    provide_context(create_signal(adapter));
 
     view! {
-        (if adapter.connected.get_clone().is_some(){
-            view!{
-                (Header(adapter.connected))
-                div(id="body-content"){
-                    // Add components here under the header
-                }
-            }
-        }else {
+        div(id="main") {
+            Router(
+                integration=HistoryIntegration::new(),
+                view=|route: ReadSignal<AppRoutes>| {
 
-            view!{
-                div(id="disconnected-content") {
-                    h1 {"Rust Wallet Adapter Demo"}
-                    div {"WALLET DISCONNECTED"}
-
-                    div {
-                        button(id="btn-primary", on:click=move |_| {
-                            show_modal.set(true)
-                        }) { "CONNECT" }
-                    }
-                }
-
-                (if show_modal.get() {
-                    let wallets = create_signal(adapter.storage.get_clone().get_wallets());
-                    let list_entries = wallets.get_clone().iter().map(|wallet| {
-                        let wallet_icon = wallet.icon().as_ref().unwrap().to_string();
-                        let wallet_name = wallet.name().to_string();
-                        let data_id = wallet_name.clone();
-
-                        view!{
-                            div(class="wallet-list-entry", data-wallet-name=data_id, on:click=move|event: MouseEvent|{
-                                let target = event.current_target().unwrap();
-                                let target_as_element = target.dyn_ref::<HtmlElement>().unwrap();
-                                let attribute_value = target_as_element.get_attribute("data-wallet-name").unwrap();
-                                wasm_bindgen_futures::spawn_local(async move {
-
-                                    let target_wallet = adapter.storage.get_clone().get_wallet(&attribute_value).unwrap();
-                                    let account_connected = target_wallet.connect().await.unwrap();
-                                    adapter.connected.set(
-                                        Some(ConnectedArgs {
-                                            connected_account: account_connected,
-                                            connected_wallet: target_wallet
-                                        })
-                                    );
-                                    show_modal.set(false);
-                                });
-                            }) {
-                                span(class="wallet-icon"){img(src=wallet_icon)}
-                                span(class="wallet-name"){ (wallet_name)}
-                            }
-                        }
-                    }).collect::<Vec<View>>();
-
-                   view!{
-                        div (id="modal-container"){
-                            div(id="modal-content") {
-                                (list_entries)
-                                button(class="btn-primary", on:click=move |_| {
-                                    show_modal.set(false)
-                                }) { "CANCEL" }
-                            }
+                    view! {
+                        div(id="main") {
+                            (Header())
+                            (match route.get_clone() {
+                                AppRoutes::Home => HomePage(),
+                                AppRoutes::NotFound => view! {
+                                    h1 { "Not Found" }
+                                },
+                            })
                         }
                     }
-                }else { view!() })
-            }
-        })
+                }
+            )
+        }
     }
+}
+
+#[derive(Route, Clone)]
+enum AppRoutes {
+    #[to("/")]
+    Home,
+    #[not_found]
+    NotFound,
 }
