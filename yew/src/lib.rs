baa21a02ae0e13b2edf80c00da836c7d9c028f1c
448 OG <superuser@448.africa>
2024-12-08 11:46:44 +0300

    Update yew template to mirror example

--- yew/src/lib.rs
+++ yew/src/lib.rs
@@ -3,8 +3,13 @@ use wasm_bindgen::prelude::*;
 mod app;
 pub(crate) use app::*;
 
+mod components;
+pub(crate) use components::*;
+
+mod views;
+pub(crate) use views::*;
+
 #[wasm_bindgen(start)]
 pub fn main() {
-
     yew::Renderer::<App>::new().render();
 }
